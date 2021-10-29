25ccdc189803e90fba43da182969bc3f1503b59d
Sustylist <sustylist.7@mail.com>
2021-10-29 15:06:50 +0800

    master

---
+++ Plugins/BM_WeaponSystem/Source/BM_WeaponSystem/Public/TargetHistory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TargetHistory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BM_WEAPONSYSTEM_API UTargetHistory : public UObject
+{
+	GENERATED_BODY()
+	
+};
